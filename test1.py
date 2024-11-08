bb8c38c05a8acd889c32c7475274abc9e3f684f2
remon168 <cogusgmf0709@gmail.com>
2024-11-08 14:28:25 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 # 1.기계학습 모델 파일 로드
-import joblib as jb
+import joblib 
 model = joblib.load('logistic_regression_model.pkl') 
 
 # 2.모델 설명
@@ -12,8 +12,8 @@ with col1:
       st.subheader('모델 설명 ')
       st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
       st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
-      st.write(' - 훈련    데이터 : *건')
-      st.write(' - 테스트 데이터 : *건')
+      st.write(' - 훈련    데이터 : 21건')
+      st.write(' - 테스트 데이터 : 8건')
       st.write(' - 모델 정확도 : 0.89')
 # 3.데이터시각화
 with col2:
